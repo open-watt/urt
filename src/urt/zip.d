c8a750a6f4c066b7d246a8afe077ada8c50ab8bf
Manu Evans <turkeyman@gmail.com>
2026-08-22 13:37:37 +1000

    zip: keep the deflate tables out of .data
    lengthc/lengtheb/distc/disteb were declared as slices, so the payloads landed
    in .data and were copied to RAM at startup along with 32 bytes of descriptors.
    They are compile-time constants; as fixed-size arrays they sit in .rodata.

--- src/urt/zip.d
+++ src/urt/zip.d
@@ -252,10 +252,10 @@ ubyte[] zlib_compress(const void[] message, int quality)
 {
     import urt.mem : memcpy, memmove;
 
-    __gshared immutable ushort[] lengthc = [ 3,4,5,6,7,8,9,10,11,13,15,17,19,23,27,31,35,43,51,59,67,83,99,115,131,163,195,227,258, 259 ];
-    __gshared immutable ubyte[] lengtheb = [ 0,0,0,0,0,0,0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4,  4,  5,  5,  5,  5,  0 ];
-    __gshared immutable ushort[] distc =   [ 1,2,3,4,5,7,9,13,17,25,33,49,65,97,129,193,257,385,513,769,1025,1537,2049,3073,4097,6145,8193,12289,16385,24577, 32768 ];
-    __gshared immutable ubyte[] disteb =   [ 0,0,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13 ];
+    __gshared immutable ushort[30] lengthc = [ 3,4,5,6,7,8,9,10,11,13,15,17,19,23,27,31,35,43,51,59,67,83,99,115,131,163,195,227,258, 259 ];
+    __gshared immutable ubyte[29] lengtheb = [ 0,0,0,0,0,0,0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4,  4,  5,  5,  5,  5,  0 ];
+    __gshared immutable ushort[31] distc =   [ 1,2,3,4,5,7,9,13,17,25,33,49,65,97,129,193,257,385,513,769,1025,1537,2049,3073,4097,6145,8193,12289,16385,24577, 32768 ];
+    __gshared immutable ubyte[30] disteb =   [ 0,0,0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13 ];
 
     int data_len = cast(int)message.length;
     ubyte* data = cast(ubyte*)message.ptr;
